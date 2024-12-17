e9422f040da932d400fad5afee77a292d3cd632f
omd34 <danielotomo34@gmail.com>
2024-12-17 04:43:52 +0100

    MAJ sidebar

--- routes/web.php
+++ routes/web.php
@@ -146,18 +146,20 @@ Route::post('/payement/investissement', [\App\Http\Controllers\PaiementControlle
 /////***** ADMIN ROUTE******/////
 use App\Http\Controllers\AdminController\AdminController;
 use App\Http\Controllers\AdminController\DepotController;
+Route::middleware('auth')->group(function () {
 
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-Route::get('/admin/recherche', [AdminController::class, 'rechercherClient'])->name('admin.recherche');
-Route::get('/admin/etat-transaction/{id}', [AdminController::class, 'etatTransaction'])->name('admin.etatTransaction');
-Route::post('/admin/changer-statut-transaction/{id}', [AdminController::class, 'changerStatutTransaction'])->name('admin.changerStatutTransaction');
-Route::get('/admin/etat_transaction/all/retrait', [AdminController::class, 'ShowAllRetrait'])->name('admin.transaction_retrait');
-Route::get('/admin/etat_transaction/all', [AdminController::class, 'ShowAllTransaction'])->name('admin.transaction_all');
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/recherche', [AdminController::class, 'rechercherClient'])->name('admin.recherche');
+    Route::get('/admin/etat-transaction/{id}', [AdminController::class, 'etatTransaction'])->name('admin.etatTransaction');
+    Route::post('/admin/changer-statut-transaction/{id}', [AdminController::class, 'changerStatutTransaction'])->name('admin.changerStatutTransaction');
+    Route::get('/admin/etat_transaction/all/retrait', [AdminController::class, 'ShowAllRetrait'])->name('admin.transaction_retrait');
+    Route::get('/admin/etat_transaction/all', [AdminController::class, 'ShowAllTransaction'])->name('admin.transaction_all');
 
 
-Route::get('/depots', [DepotController::class, 'index'])->name('admin.depots');
-Route::post('/depots', [DepotController::class, 'store'])->name('admin.store_depot');
-Route::get('/depots/{id}', [DepotController::class, 'show'])->name('admin.show_depot');
-Route::get('/depots/{id}/edit', [DepotController::class, 'edit'])->name('admin.edit_depot');
-Route::put('/depots/{id}', [DepotController::class, 'update'])->name('admin.update_depot');
-Route::delete('/depots/{id}', [DepotController::class, 'destroy'])->name('admin.destroy_depot');
+    Route::get('/depots', [DepotController::class, 'index'])->name('admin.depots');
+    Route::post('/depots', [DepotController::class, 'store'])->name('admin.store_depot');
+    Route::get('/depots/{id}', [DepotController::class, 'show'])->name('admin.show_depot');
+    Route::get('/depots/{id}/edit', [DepotController::class, 'edit'])->name('admin.edit_depot');
+    Route::put('/depots/{id}', [DepotController::class, 'update'])->name('admin.update_depot');
+    Route::delete('/depots/{id}', [DepotController::class, 'destroy'])->name('admin.destroy_depot');
+});
