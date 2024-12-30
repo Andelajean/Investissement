52b151a470e10c1ee77baa7b6b02e14e7e211dda
KOUOMO Merveille <merveillekouomo@gmail.com>
2024-12-30 23:11:10 +0100

    patenariats corrige

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -535,38 +535,95 @@
 
 <!--start partenaire-->
 <section class="why_section layout_padding">
-  <div class="container">
-      <div class="heading_container heading_center">
-          <h2 class="pb-5 mb-3">
-              Nos<span> Partenaires</span>
-          </h2>
-          <h5 class="mb-5">Chez <b> Global Investissement Trading </b>, nous croyons que la collaboration est la clé pour offrir des solutions d'investissement performantes et durables. C'est pourquoi nous avons établi des partenariats stratégiques avec des institutions financières renommées, des entreprises technologiques innovantes et des experts en finance.</h5>
-      </div>
+    <div class="container">
+        <div class="heading_container heading_center">
+            <h2 class="pb-5 mb-3">
+                Nos<span> Partenaires</span>
+            </h2>
+            <h5 class="mb-5">
+                Chez <b>Global Investissement Trading</b>, nous croyons que la collaboration est la clé pour offrir des solutions d'investissement performantes et durables. 
+                C'est pourquoi nous avons établi des partenariats stratégiques avec des institutions financières renommées, des entreprises technologiques innovantes et des experts en finance.
+            </h5>
+        </div>
 
-      <div class="row text-center">
-          <!-- Partenaire 1 -->
-          <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
-              <div class="img-box d-flex justify-content-center">
-                  <img src="image/afbank.jpeg" alt="afriklanf first bank" class="img-fluid" style="width: 80px; height: 80px;">
-              </div>
-          </div>
-          <!-- Partenaire 2 -->
-          <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
-              <div class="img-box d-flex justify-content-center">
-                  <img src="image/bicec.png" alt="bicec bank" class="img-fluid" style="width: 80px; height: 80px;">
-              </div>
-          </div>
-          <!-- Partenaire 3 -->
-          <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
-              <div class="img-box d-flex justify-content-center">
-                  <img src="image/cca.jpeg" alt="cca bank" class="img-fluid" style="width: 80px; height: 80px;">
-              </div>
-          </div>
-          <!-- Ajoutez les autres images de partenaires de la même manière -->
-      </div>
-  </div>
+        <div class="row text-center g-4">
+            <!-- Partenaire 1 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/afbank.jpeg" alt="Afrikland First Bank" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 2 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/bicec.png" alt="BICEC Bank" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 3 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/cca.jpeg" alt="CCA Bank" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 4 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/uba.jpeg" alt="UBA Bank" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 5 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/scb.png" alt="SCB" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 6 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/cbc.jpeg" alt="CBC" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 7 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/ecobank.jpeg" alt="Ecobank" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 8 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/sgc.png" alt="SGC" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 9 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/bgc.png" alt="BGC" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 10 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/sc.jpg" alt="Standard Chartered" class="img-fluid" style="width: 100px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 11 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/momo.png" alt="Mobile Money" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+            <!-- Partenaire 12 -->
+            <div class="col-4 col-sm-3 col-md-2 mb-4">
+                <div class="img-box d-flex justify-content-center">
+                    <img src="image/om.jpeg" alt="Orange Money" class="img-fluid" style="width: 80px; height: 80px;">
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <!--end partenaire-->
+
 
 
     @endsection
