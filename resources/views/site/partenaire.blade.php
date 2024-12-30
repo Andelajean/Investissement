ddf82b1cc70b5633e06991f50d4b42fe1bd95399
KOUOMO Merveille <merveillekouomo@gmail.com>
2024-12-30 22:53:45 +0100

    patenariats corrige

--- resources/views/site/partenaire.blade.php
+++ resources/views/site/partenaire.blade.php
@@ -13,74 +13,31 @@
             <h5 class="mb-5">Chez <b> Global Investissement Trading </b>, nous croyons que la collaboration est la clé pour offrir des solutions d'investissement performantes et durables. C'est pourquoi nous avons établi des partenariats stratégiques avec des institutions financières renommées, des entreprises technologiques innovantes et des experts en finance.</h5>
         </div>
 
-        <div class="row">
-            <div class="col-1">
-                <div class="img-box">
+        <div class="row text-center">
+            <!-- Partenaire 1 -->
+            <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
+                <div class="img-box d-flex justify-content-center">
                     <img src="image/afbank.jpeg" alt="afriklanf first bank" class="img-fluid" style="width: 80px; height: 80px;">
                 </div>
             </div>
-            <div class="col-1">
-                <div class="img-box">
+            <!-- Partenaire 2 -->
+            <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
+                <div class="img-box d-flex justify-content-center">
                     <img src="image/bicec.png" alt="bicec bank" class="img-fluid" style="width: 80px; height: 80px;">
                 </div>
             </div>
-            <div class="col-1">
-                <div class="img-box">
+            <!-- Partenaire 3 -->
+            <div class="col-4 col-sm-3 col-md-2 g-3 mx-auto my-3">
+                <div class="img-box d-flex justify-content-center">
                     <img src="image/cca.jpeg" alt="cca bank" class="img-fluid" style="width: 80px; height: 80px;">
                 </div>
             </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/uba.jpeg" alt="uba bank" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/scb.png" alt="scb" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/cbc.jpeg" alt="cbc" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/ecobank.jpeg" alt="ecobank" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/sgc.png" alt="sgc" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/bgc.png" alt="bgc" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/sc.jpg" alt="Standard Chartered" class="img-fluid" style="width: 100px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/momo.png" alt="mobile money" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            <div class="col-1">
-                <div class="img-box">
-                    <img src="image/om.jpeg" alt="orange money" class="img-fluid" style="width: 80px; height: 80px;">
-                </div>
-            </div>
-            
+            <!-- Ajoutez les autres images de partenaires de la même manière -->
         </div>
     </div>
-
-
 </section>
 <!--end partenaire-->
+
 
 @endsection 
 
